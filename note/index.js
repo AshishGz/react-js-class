7a24194794553d04360991cd098c5a7668d982ae
ashishgautam <agautam@hamropatro.com>
2020-09-09 08:26:52 +0545

    notes feature

---
+++ note/index.js
@@ -0,0 +1,31 @@
+let NOTES_KEY='user_note';
+window.onload=function () {
+   displayNote();
+};
+var notes=JSON.parse(localStorage.getItem(NOTES_KEY));
+function saveNote() {
+    var note=document.getElementById('note').value;
+    notes.push(note);
+    localStorage.setItem(NOTES_KEY,JSON.stringify(notes));
+    displayNote();
+}
+
+function displayNote() {
+    var notes=JSON.parse(localStorage.getItem(NOTES_KEY));
+    var notesDiv='';
+    notes.forEach(function (value,index) {
+        if(value) {
+            notesDiv = notesDiv + ' <div class="displayNote">\n' +
+                '        <div>' + value + '</div>\n' +
+                '        <button id="remove" onclick="deleteNote(' + index + ')">Remove</button>\n' +
+                '    </div>';
+        }
+    });
+    document.getElementById('display').innerHTML=notesDiv;
+}
+function deleteNote(id) {
+    var notes=JSON.parse(localStorage.getItem(NOTES_KEY));
+     delete notes[id];
+     localStorage.setItem(NOTES_KEY,JSON.stringify(notes));
+     displayNote();
+}
